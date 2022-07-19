f3484ff34ac8ba9400fbf2ced313760e4f0ffe58
Serhii <havryliuk.serhii@icloud.com>
2022-07-19 22:08:16 +0300

    delete spec files, change port on 3001, add env varuable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
-import {MongooseModule} from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestProductModule } from './test-product/test-product.module';
 import { AppGateway } from './app.gateway';
 
 @Module({
-  imports: [TestProductModule, MongooseModule.forRoot('mongodb+srv://serhii:12345@cluster0.hr9dt.mongodb.net/?retryWrites=true&w=majority')],
-  controllers: [AppController],
-  providers: [AppService, AppGateway],
+	imports: [
+		TestProductModule,
+		ConfigModule.forRoot(),
+		MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.hr9dt.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`),
+	],
+	controllers: [
+		AppController]
+	,
+	providers: [
+		AppService,
+		AppGateway
+	],
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useStaticAssets(join(__dirname, '..', 'static'));
-  enableCors
-  await app.listen(process.env.PORT || 3000);
+  app.enableCors();
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
