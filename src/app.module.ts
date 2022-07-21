2b315b85bb7a2c109577eaa401fa730add88390d
OlegVBel <olegbelous2002@gmail.com>
2022-07-21 11:00:32 +0300

    saving

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import {MongooseModule} from "@nestjs/mongoose";
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestProductModule } from './test-product/test-product.module';
+import { GoodController } from './good/good.controller';
 
 @Module({
   imports: [TestProductModule, MongooseModule.forRoot('mongodb+srv://serhii:12345@cluster0.hr9dt.mongodb.net/?retryWrites=true&w=majority')],
-  controllers: [AppController],
+  controllers: [AppController, GoodController],
   providers: [AppService],
 })
 export class AppModule {}
---
+++ src/good/good.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('good')
+export class GoodController {}
