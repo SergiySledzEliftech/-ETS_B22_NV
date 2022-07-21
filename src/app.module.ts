3b8b8d5e5772c538691f4560ad29f85e12cd46cf
Dmytro Iudin <dmtry.iudin@gmail.com>
2022-07-21 20:09:39 +0300

    top 10 added

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { TestProductModule } from './test-product/test-product.module';
 import { NewsModule } from './news/news.module';
 import { AppGateway } from './app.gateway';
 import { StatisticsModule } from './statistics/statistics.module';
+import { Top10Controller } from './top10/top10.controller';
+import { Top10Module } from './top10/top10.module';
 
 @Module({
 	imports: [
@@ -15,9 +17,10 @@ import { StatisticsModule } from './statistics/statistics.module';
 		ConfigModule.forRoot(),
 		MongooseModule.forRoot(`mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@cluster0.hr9dt.mongodb.net/${process.env.DATABASE_NAME}?retryWrites=true&w=majority`),
 		StatisticsModule,
+		Top10Module,
 	],
 	controllers: [
-		AppController,]
+		AppController]
 	,
 	providers: [
 		AppService,
