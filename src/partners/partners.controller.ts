1e1867d4805b8cdc35bfdb6f623df320897e7733
unknown <libraolena@gmail.com>
2022-07-31 16:29:58 +0300

    changes publicity of routes

--- src/partners/partners.controller.ts
+++ src/partners/partners.controller.ts
@@ -1,7 +1,9 @@
 import { Controller, Get } from '@nestjs/common';
 import { PartnersService } from './partners.service';
 import { Partner } from './scemas/partners.schema';
+import {Public} from '../auth/auth.controller';
 
+@Public()
 @Controller('partners')
 export class PartnersController {
     constructor(private readonly partnersService:PartnersService) {}
