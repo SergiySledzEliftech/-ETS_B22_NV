5171361751a6bc0b9a72d1f7f850c5a7dbf01450
unknown <libraolena@gmail.com>
2022-07-31 20:34:25 +0300

    changed routes privacy

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,23 @@
+import {IsEmpty, Matches} from 'class-validator';
+
 export class CreateUserDto {
+	@IsEmpty()
+	@Matches(/^[a-zA-Z]*$/)
 	firstName: string;
+
+	@IsEmpty()
+	@Matches(/^[a-zA-Z]*$/)
 	lastName: string;
+
+	@IsEmpty()
+	@Matches(/\(?([0-9]{3})\)?([ .-]?)([0-9]{3})\2([0-9]{4})/)
 	phone: number;
+
+	@IsEmpty()
+	@Matches(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g)
 	email: string;
+
+	@IsEmpty()
+	@Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
 	passHash: string;
 }
--- src/users/dto/existing-user.dto.ts
+++ src/users/dto/existing-user.dto.ts
@@ -1,4 +1,11 @@
+import {IsEmpty, Matches} from 'class-validator';
+
 export class ExistingUserDto {
+	@IsEmpty()
+	@Matches(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g)
 	email: string;
+
+	@IsEmpty()
+	@Matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
 	passHash: string;
 }
